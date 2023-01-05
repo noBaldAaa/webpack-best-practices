039ba897a7dd741bf7483402e66c9c32fe896df9
wxy <102939294+noBaldAaa@users.noreply.github.com>
2023-01-05 23:15:12 +0800

    npm init

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,11 @@
 //
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js", //定义入口
+  output: {
+    //告诉 webpack 在哪里输出它所创建的 bundle，以及如何命名这些文件
+    filename: "bundle.js", //定义打包后的文件名称
+    path: path.resolve(__dirname, "./dist"), //必须是绝对路径
+  },
+};
